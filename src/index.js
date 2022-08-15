35f64da9a8156017070a10a87da93c0ef7b35d25
aron-helu <aronmilahela@gmail.com>
2022-08-15 13:21:53 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, HashRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
 );
