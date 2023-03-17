1df8430bc2eb9c0318a01cd15f357cc6a28eaf53
aron-helu <aronmilahela@gmail.com>
2023-03-18 02:07:05 +0300

    Fix linters and add proptypes

--- src/Components/ExpenseTracker/NewExpense/NewExpense.js
+++ src/Components/ExpenseTracker/NewExpense/NewExpense.js
@@ -1,16 +1,19 @@
+import PropTypes from 'prop-types';
 import Card from '../../UI/Card';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = (props) => {
+const NewExpense = ({ onAddNewExpense }) => {
   const saveNewExpense = (enteredExpense) => {
     const expenseData = {
       ...enteredExpense,
       id: Math.random().toLocaleString(),
     };
-    props.onAddNewExpense(expenseData);
+    onAddNewExpense(expenseData);
   };
   return <Card className="new-expense"><ExpenseForm onSaveNewExpense={saveNewExpense} /></Card>;
 };
-
+NewExpense.propTypes = {
+  onAddNewExpense: PropTypes.func.isRequired,
+};
 export default NewExpense;
