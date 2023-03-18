7e86967c217ca12f3d6818eee20cdbf7ecc6c6cc
aron-helu <aronmilahela@gmail.com>
2023-03-18 04:48:17 +0300

    Add dummy filter

--- src/Components/ExpenseTracker/Expense/ExpenseList.js
+++ src/Components/ExpenseTracker/Expense/ExpenseList.js
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import ExpenseItem from './ExpenseItem';
 import Card from '../../UI/Card';
 import './ExpenseList.css';
+import FilterExpense from '../FilterExpense/FilterExpense';
 
-const ExpenseList = ({ data }) => (
-  <Card className="expense-list">
-    {data.map(({
-      id, title, amount, date,
-    }) => (
-      <ExpenseItem key={id} title={title} amount={amount} date={date} />
-    ))}
-  </Card>
-);
+const ExpenseList = ({ data }) => {
+  const [filterExpense, setFilterExpense] = useState('2023');
+  const addFilterChangeHandler = (selectYear) => {
+    setFilterExpense(selectYear);
+  };
+  return (
+    <Card className="expense-list">
+      <FilterExpense
+        selected={filterExpense}
+        onChangeFilter={addFilterChangeHandler}
+      />
+      {data.map(({
+        id, title, amount, date,
+      }) => (
+        <ExpenseItem key={id} title={title} amount={amount} date={date} />
+      ))}
+    </Card>
+  );
+};
 
 ExpenseList.propTypes = {
   data: PropTypes.arrayOf(
